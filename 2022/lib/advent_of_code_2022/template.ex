9097fb15563ec45e6d2e7a5c8ccdc98b3bfd3208
Christoph Grothaus <me@cgrothaus.de>
2022-12-02 16:01:16 +0100

    feat: Add template

---
+++ 2022/lib/advent_of_code_2022/template.ex
@@ -0,0 +1,10 @@
+defmodule AdventOfCode.AoC2022.Template do
+  def input(), do: File.read!("inputs/day01.txt")
+  def example_input(), do: File.read!("inputs/day01_example.txt")
+
+  def part1(_input) do
+  end
+
+  def part2(_input) do
+  end
+end
---
+++ lib/advent_of_code/aoc2022/template.ex
@@ -0,0 +1,10 @@
+defmodule AdventOfCode.AoC2022.Template do
+  def input(), do: File.read!("inputs/2022/day1.txt")
+  def example_input(), do: File.read!("inputs/2022/day1_example.txt")
+
+  def part1(input) do
+  end
+
+  def part2(input) do
+  end
+end
